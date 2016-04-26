9e537a9610f98da4455e5657b2c707cc56be54c7
Giorgio Salluzzo <giorgio.salluzzo@planet.com>
2016-04-26 14:56:52 +0200

    Version '0.1.5'.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requirements = open(os.path.join(os.path.dirname(__file__), 'test_requireme
 
 setup(
     name='Flask-Breathalyzer',
-    version='0.1.4',
+    version='0.1.5',
     license='BSD',
     url='https://github.com/mindflayer/flask-breathalyzer',
     author='Giorgio Salluzzo',
