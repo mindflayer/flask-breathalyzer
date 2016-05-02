e21a7a927d12748f8a821a4c5ef02cf13b0b533c
Giorgio Salluzzo <giorgio.salluzzo@gmail.com>
2016-05-02 19:21:18 +0200

    Moving to fixtures.

--- tests/test_breathalyzer.py
+++ tests/test_breathalyzer.py
@@ -12,30 +12,41 @@ def app():
     return Flask(__name__)
 
 
-def test_initapp(app):
-    @app.route('/', methods=['GET', 'POST'])
-    def boom():
-        1/0
+@pytest.fixture
+def test_client(app):
+    return app.test_client()
 
-    test_client = app.test_client()
+
+@pytest.fixture
+def path_info():
+    return '/boom'
+
+
+def test_failing_initapp(app, test_client, path_info):
+    @app.route(path_info)
+    def boom():
+        1 / 0
 
     Breathalyzer(app)
 
     try:
-        test_client.get('/')
+        test_client.get(path_info)
     except Exception as e:
         assert type(e) == datadog.api.exceptions.ApiNotInitialized
 
     Breathalyzer(app, api_key='api_key', app_key='app_key')
 
-    header = 'Accept'
-    html = 'text/html'
     try:
-        test_client.post('/', data=json.dumps(dict(h=html)), headers=dict(h=html))
+        test_client.get(path_info)
     except Exception as e:
         assert type(e) == ValueError
         assert 'Invalid JSON response' in e.args[0]
         assert 'Invalid API Key' in e.args[0]
+
+def test_succesful_event(app, test_client, path_info):
+    @app.route(path_info, methods=['GET', 'POST'])
+    def boom():
+        1 / 0
 
     # from http://docs.datadoghq.com/api/
     options = {
@@ -43,6 +54,9 @@ def test_initapp(app):
         'app_key': '87ce4a24b5553d2e482ea8a8500e71b8ad4554ff'
     }
 
+    header = 'Accept'
+    html = 'text/html'
+    headers = {header: html}
     to_ban = ('to ban', 'Guido Van Rossum')
     to_find = ('to find', 'Raymond Hettinger')
     data = dict((to_ban, to_find))
@@ -50,7 +64,7 @@ def test_initapp(app):
     d_blacklist = ['/{0}'.format(to_ban[0])]
     ba = Breathalyzer(app, headers_blacklist=h_blacklist, data_blacklist=d_blacklist, **options)
     assert ba.last_event_id is None
-    response = test_client.post('/', data=json.dumps(data), content_type='application/json')
+    response = test_client.post(path_info, data=json.dumps(data), content_type='application/json', headers=headers)
     assert response.status == '500 INTERNAL SERVER ERROR'
     assert b'<title>500 Internal Server Error</title>' in response.data
     assert response.mimetype == 'text/html'
